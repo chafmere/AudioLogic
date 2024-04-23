3dbd843e4ef46bb90f1ca181d3f3dd34cd7245d2
chafmere <isaac_1990@hotmail.com>
2024-04-23 21:43:54 +1000

    Add Read me

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
+
+<img src="icon.svg" width ="128" height = "128">
+
 # AudioLogic
-An Audio Log plugin for Godot 4.X
+
+### An Audio Log plugin for Godot 4.X
+
+AudioLogic is an addon for [Godot 4](https://godotengine.org/) that provides an easy way to add Audio logs into your game.
+
+This addon is not yet available in the Asset Library and is in the early stages of development.
+
+[![Patreon](https://img.shields.io/badge/Patreon-Support%20this%20Project-%23f1465a?style=for-the-badge)](patreon.com/ChaffGames) [![Discord](https://img.shields.io/discord/865048184160911421?style=for-the-badge&logo=Discord&label=Discord)](https://discord.gg/Exzd8QmKrU)
+
+## Documentation
+
+Coming Soon...
+
--- addons/audiologic/Example/AudioCollectable.gd
+++ addons/audiologic/Example/AudioCollectable.gd
@@ -1,10 +1,14 @@
 extends AudioLogCollectable
 
-@onready var disk: MeshInstance3D = $disk
-const HIGHLIGHTER = preload("res://addons/audiologic/Example/Example World/highlighter.tres")
+@onready var radio_base: MeshInstance3D = $"radio base"
+@onready var radio_buttons: MeshInstance3D = $radio_buttons
+
+const HIGHLIGHTER = preload("res://addons/audiologic/Extras/highlighter.tres")
 
 func _on_detected(_is_detected: bool) -> void:
 	if _is_detected:
-		disk.get_active_material(0).set_next_pass(HIGHLIGHTER)
+		radio_base.get_active_material(0).set_next_pass(HIGHLIGHTER)
+		radio_buttons.get_active_material(0).set_next_pass(HIGHLIGHTER)
 	else:
-		disk.get_active_material(0).next_pass = null
+		radio_base.get_active_material(0).next_pass = null
+		radio_buttons.get_active_material(0).next_pass = null
